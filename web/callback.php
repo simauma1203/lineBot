c02a2704313ec11faf90f792b859b705b908a301
simauma1203 <20992019+simauma1203@users.noreply.github.com>
2018-04-19 19:21:00 +0900

    update

--- web/callback.php
+++ web/callback.php
@@ -523,7 +523,7 @@ if($text=="ヴァネロピ"){
 if($text=="ビンビン"){
   $response_format_text = [
     "type" => "audio",
-    "originalContentUrl" => $_SERVER['SERVER_NAME']."/anta.mp3",
+    "originalContentUrl" => $_SERVER['SERVER_NAME']."/web/anta.mp3",
     "duration"=>"22000"
   ];
 }
