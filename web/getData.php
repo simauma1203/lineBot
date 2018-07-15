e4abe675e8915b338cbe4b18eda4c46b5e7acd45
simauma1203 <20992019+simauma1203@users.noreply.github.com>
2018-07-16 08:51:00 +0900

    update

--- web/getData.php
+++ web/getData.php
@@ -7,17 +7,23 @@ if($postText==null){
     //exit;
 }
 
-$accessToken="v61upqQUN/oE4yiwgij6n9IbIy8PbStfbvan2xrNlgg2OFswMK7XLBLO4rlyjmk30/a3EkNtwVqIcSMOOVgZMQlhlpF6hxuJXG6GugC9s/X008nYQ8s04Z38eb+l3zOaeIaUPWmQCv6ybAjtrIHdVAdB04t89/1O/w1cDnyilFU=";
-$channelSecret="dfd80f0736d4a20a2114cc6d4babcd5f";
+$accessToken = 'v61upqQUN/oE4yiwgij6n9IbIy8PbStfbvan2xrNlgg2OFswMK7XLBLO4rlyjmk30/a3EkNtwVqIcSMOOVgZMQlhlpF6hxuJXG6GugC9s/X008nYQ8s04Z38eb+l3zOaeIaUPWmQCv6ybAjtrIHdVAdB04t89/1O/w1cDnyilFU=';
+$channelSecret='dfd80f0736d4a20a2114cc6d4babcd5f';//lineCS
 
+$url=parse_url(getenv('DATABASE_URL'));
+$dsn=sprintf('pgsql:host=%s;dbname=%s',$url['host'],substr($url['path'],1));
+$pdo=new PDO($dsn,$url['user'],$url['pass']);
+
+
+$groupid="C8727e59e0381bc8c6a7fef3f7f8e4cf2";
 function push($gId,$message){
     global $accessToken,$channelSecret;  
     $url = 'https://api.line.me/v2/bot/message/push';
     // データの受信(するものないので不要?)
-    //$raw = file_get_contents('php://input');
-    //$receive = json_decode($raw, true);
+    $raw = file_get_contents('php://input');
+    $receive = json_decode($raw, true);
     // イベントデータのパース(不要？)
-    //$event = $receive['events'][0];
+    $event = $receive['events'][0];
     // ヘッダーの作成
     $headers = ['Content-Type: application/json','Authorization: Bearer '.$accessToken];
     // 送信するメッセージ作成
@@ -34,33 +40,7 @@ function push($gId,$message){
   curl_setopt_array($curl, $options);
   curl_exec($curl);
   curl_close($curl);
-  
-  }
-
-
-
-    $message = array(
-      "type" => "text",
-      "text" => $postText
-    );
-    //push("C8727e59e0381bc8c6a7fef3f7f8e4cf2",$message);
-  
-
-/*
-try{
-    $pdo=new PDO('mysql:host=us-cdbr-iron-east-04.cleardb.net;dbname=heroku_7f49637117262f3;charset=utf8',
-    'b22d7c8e9a3b75', '6a6806d7');
-    print("success");
-}catch(PDOException $e){
-    print("err");
-}*/
-print("a");
-
-$url=parse_url(getenv('DATABASE_URL'));
-$dsn=sprintf('pgsql:host=%s;dbname=%s',$url['host'],substr($url['path'],1));
-$pdo=new PDO($dsn,$url['user'],$url['pass']);
-
-print("i");
+}
 
 $maxCnt=5;
 $cnt=0;
@@ -74,15 +54,14 @@ while($row = $stmt -> fetch(PDO::FETCH_ASSOC)) {
     $cnt++;
     if($cnt==$maxCnt)break;
 }
-print("u");
 
-print(PHP_EOL."-resData-".PHP_EOL);
+//print(PHP_EOL."-resData-".PHP_EOL);
 
 
 $resultData="contentFromPHP";
 
 //読み込んだデータをjson形式で端末に送信する
-header('Content-type: application/json');
-print json_encode($resultData);
+header('Content-type: application/json;');
+print json_encode($row);
 
-print("e");
+//print("e");
