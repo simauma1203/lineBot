014357b959cfd4d868f0d8693f91eaa27f5c58f4
simauma1203 <20992019+simauma1203@users.noreply.github.com>
2020-02-06 02:09:42 +0900

    update

--- web/tamaroning.php
+++ web/tamaroning.php
@@ -41,7 +41,9 @@ if(strpos($weather_stat,'晴れ') !== false){
 if(strpos($weather_stat,'雨') !== false){
   $weather_char="☔️";
 }
-
+if(22<=h || h<=3){
+  $weather_char="🌟";
+}
 
 $name=$time_char."まろん".$weather_char."(".$weather_stat.")";
 
